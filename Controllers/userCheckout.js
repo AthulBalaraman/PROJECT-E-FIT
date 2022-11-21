517679eaff975d1e8e527a81eac4b1896128dd73
AthulBalaraman <athul2522001@gmail.com>
2022-11-21 16:46:29 +0530

    total reached payment page

--- Controllers/userCheckout.js
+++ Controllers/userCheckout.js
@@ -6,6 +6,7 @@ const wishListModel = require('../Model/userWishListModel')
 const couponModel = require('../Model/userCouponModel')
 
 const showCheckOutPage = async (req, res) => {
+  console.log('***************************',req.query.finalTotal)
   let products = await cartModel.getCartProducts(req.session.user._id)
   let cartCount = null;
   let wishListCount = null
@@ -14,7 +15,8 @@ const showCheckOutPage = async (req, res) => {
     wishListCount = await wishListModel.getWishListCount(req.session.user._id)
   }
   let total = await checkOut.getTotalAmount(req.session.user._id)
-  
+  let finalTotal = Math.round(req.query.finalTotal)
+
   category.displayCategory().then((category) => {
     let userData = req.session.user;
     res.render("user/proceedToCheckOutPage", {
@@ -24,40 +26,19 @@ const showCheckOutPage = async (req, res) => {
       cartCount,
       category,
       products,
-      total,
+      finalTotal,
       wishListCount
     });
   });
 };
 
 const showCheckingOutPage = async(req,res)=>{
-  let products = await cartModel.getCartProducts(req.session.user._id)
-  let cartCount = null;
-  let wishListCount = null
-  if (req.session.user) {
-    cartCount = await cartModel.getCartCount(req.session.user._id);
-    wishListCount = await wishListModel.getWishListCount(req.session.user._id)
-  }
   let finalTotal = req.body.TOTAL//cart total
   let details = req.body
   if(details.couponCode==='')
   {
-    finalTotal = details.TOTAL
-    // category.displayCategory().then((category) => {
-    //   let userData = req.session.user;
-
-    //   res.render("user/proceedToCheckOutPage", {
-    //     admin: false,
-    //     user: true,
-    //     userData,
-    //     cartCount,
-    //     category,
-    //     products,
-    //     finalTotal,
-    //     wishListCount
-    //   });
-    // });
-    
+    finalTotal = details.TOTAL  
+    res.json(finalTotal)
   }
   else{
     console.log('this is checkingout page req.body details',details);
@@ -68,41 +49,15 @@ const showCheckingOutPage = async(req,res)=>{
       await couponModel.getDiscount(couponDetails, details.TOTAL).then((response) => {
         console.log('########################################',response);
         finalTotal = response.discountedTotal
-        console.log('this is final total',finalTotal);
-        
-      //   category.displayCategory().then((category) => {
-      //     let userData = req.session.user;
-          
-      //     res.render("user/proceedToCheckOutPage", {
-      //       admin: false,
-      //       user: true,
-      //       userData,
-      //       cartCount,
-      //       category,
-      //       products,
-      //       finalTotal,
-      //       wishListCount
-      //     });
-      // });
+        console.log('this is final total',response);
+        res.json(response.discountedTotal)
+
       });
     }
     else
     {
-      finalTotal = details.TOTAL
-      // category.displayCategory().then((category) => {
-      //   let userData = req.session.user;
-      //   res.render("user/proceedToCheckOutPage", {
-      //     admin: false,
-      //     user: true,
-      //     userData,
-      //     cartCount,
-      //     category,
-      //     products,
-      //     finalTotal,
-      //     wishListCount
-      //   });
-      // });
-      
+      finalTotal = details.TOTAL 
+      res.json(finalTotal)     
     }
   }
 }
